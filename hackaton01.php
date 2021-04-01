37b03058ae862c928e5aa57ac9e0deb7a8a12045
Kat-ou <catherine.detournay2020@campus-eni.fr>
2021-04-01 16:58:39 +0200

    Initial commit

---
+++ hackaton01.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>Question</h2>
+<br>
+<?php
+
+$row = 1;
+if (($handle = fopen("servers/servers_catalog.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num champs à la ligne $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
+?>
+
+</body>
+</html>
