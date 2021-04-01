8ef93e0252566d010a3ab384d490af7986dcd952
Kat-ou <catherine.detournay2020@campus-eni.fr>
2021-04-01 17:21:33 +0200

    test commit

--- hackaton01.php
+++ hackaton01.php
@@ -13,18 +13,94 @@
 <br>
 <?php
 
-$row = 1;
-if (($handle = fopen("servers/servers_catalog.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        echo "<p> $num champs à la ligne $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
+class Main
+
+{
+    const ANNEE = 6;
+
+    public function getServers()
+    {
+        $row = 1;
+        if (($handle = fopen("C:\Users\LANNEAU JJ\PhpstormProjects\Hackathome\servers\servers_catalog.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $catalog[] = $data;
+
+            }
+            fclose($handle);
+            return $catalog;
+
         }
+
     }
-    fclose($handle);
+
+    public function getServices($inputCsv)
+    {
+
+        $row = 1;
+        if (($handle = fopen($inputCsv, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $catalog[] = $data;
+
+            }
+            fclose($handle);
+            return $catalog;
+        }
+
+    }
+
+    public function getServicesSums($inputCsv)
+    {
+        $services = $this->getServices($inputCsv);
+        $serviceMax = [];
+
+        foreach ($services as $service) {
+            for ($i = 1; $i < count($service); $i++) {
+                $serviceMax[$i] += $service[$i];
+            }
+        }
+        return $serviceMax;
+    }
+
+    public function getSolution($inputCsv)
+    {
+        $servers = $this->getServers();
+        $serviceMax = $this->getServicesSums($inputCsv);
+
+        $serverSolution = "";
+        $serverSolutionCo2 = 1000000;
+
+
+        foreach ($servers as $server) {
+
+            $serverCapa = $server[3];
+            $serverRam = $server[4];
+            $serverCore = $server[5];
+
+            settype($serverCapa, 'integer');
+            settype($serverRam, 'integer');
+            settype($serverCore, 'integer');
+
+            if ($serverCapa >= $serviceMax[1] && $serverRam >= $serviceMax[2] && $serverCore >= $serviceMax[3]) {
+                $co2 = $server[1] + $server[2] * self::ANNEE;
+                var_dump($co2);
+                if ($serverSolutionCo2 > $co2) {
+                    $serverSolutionCo2 = $co2;
+                    $serverSolution = $server[0];
+                }
+            }
+
+        }
+        return $serverSolution;
+    }
 }
+
+$main = new Main();
+$inputCsv = "C:\Users\LANNEAU JJ\PhpstormProjects\Hackathome\inputs\ctstfr0280_input_1.csv";
+
+$serverSolution = $main->getSolution($inputCsv);
+var_dump($serverSolution);
+
+
 ?>
 
 </body>
